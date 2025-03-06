21e10af8ed8588109c40e4323aa1bef6448ef81a
mazeem77 <mazeem8856@gmail.com>
2025-03-07 02:37:06 +0500

    incident

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -1,9 +1,18 @@
+interface UserInterface {
+    id: string;
+    name: string;
+    email: string;
+    mediaId: string;
+    imageUrl: string;
+    token: string;
+    role: string;
+}
 
 
 // Type for the login response
 interface LoginResponseData {
     status: boolean;
     message: string;
-    user: User;
+    user: UserInterface;
     token: string;
 }
