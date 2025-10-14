8db1017eea7ea6680dc6430b42a5f530cfba66bb
iqra-shahzadi <iqrashahzadi828@gmail.com>
2025-10-14 17:00:37 +0500

    search focus fixed

--- src/app/dashboard/incidents/page.tsx
+++ src/app/dashboard/incidents/page.tsx
@@ -118,7 +118,7 @@ export default function IncidentsList() {
         <input
           type="text"
           placeholder="Search incidents..."
-          className="my-2 w-full rounded-l-md border border-gray-300 px-2 py-3 text-sm shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white"
+          className="my-2 w-full rounded-l-md border border-gray-300 px-2 py-3 text-sm shadow-sm placeholder:text-neutral-400 focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 group-hover/input:shadow-none dark:border-gray-600 dark:bg-gray-700 dark:text-white"
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <Dropdown
